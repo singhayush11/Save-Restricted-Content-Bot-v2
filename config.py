91c5a3443d0045d406f14ffe9a55ea8aecc47d89
singhayush11 <ayushsk058@gmail.com>
2025-04-28 16:21:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,11 +3,11 @@
 
 from os import getenv
 
-API_ID = int(getenv("API_ID", "27713051"))
-API_HASH = getenv("API_HASH", "952347c8eeaba7a7f20d7f92817fc4f1")
+API_ID = int(getenv("API_ID", ""))
+API_HASH = getenv("API_HASH", "")
 BOT_TOKEN = getenv("BOT_TOKEN", "")
-OWNER_ID = int(getenv("OWNER_ID", "7507502628"))
-MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://Yashwork1999:rvl8goTbWtCfrIu6@cluster0.godehne.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_ID = int(getenv("OWNER_ID", ""))
+MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "")
 LOG_GROUP = int(getenv("LOG_GROUP", ""))
 FORCESUB = getenv("FORCESUB", "")
 DEFAULT_SESSION = getenv("DEFAULT_SESSION", "") # this is jkust to help if you dont want to force your bot user to login or if they not interested
